70326bb288e2484805ad163d72c19c81a7ede192
hyrozen88 <bastini@hotmail.fr>
2017-11-12 14:28:53 +0100

    map repositionning

--- js/directparlement.js
+++ js/directparlement.js
@@ -511,7 +511,7 @@
     var params=params="refine.ref="+num_deptmt;
     if(num_circo!=null)
         params="refine.ref="+num_deptmt+'-'+num_circo;
-    document.getElementById("map").innerHTML='<iframe src="https://public.opendatasoft.com/explore/embed/dataset/circonscriptions-legislatives-2017/map/?'+params+'&basemap=mb-931882" width="800" height="600" style="position: relative;left:-40px;top:-20px;"></iframe>';
+    document.getElementById("map").innerHTML='<iframe src="https://public.opendatasoft.com/explore/embed/dataset/circonscriptions-legislatives-2017/map/?'+params+'&basemap=mb-931882" width="800" height="600" style="position: relative;left:-40px;top:-60px;"></iframe>';
 }
 
   $(document).ready(function(){
